1277952504b2107783c56916508a176eeb3e7567
winthihasoe <winthihasoe@gmail.com>
2026-01-21 18:46:01 +0630

    added favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,17 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Sample School Websites by IHTechno</title>
+        <meta name="description" content="IHTechno specializes in website and mobile application development, offering innovative solutions to enhance your digital presence.">
+        <meta property="og:title" content="Sample School Websites by IHTechno | Website and Mobile Application Development">
+        <meta property="og:description" content="IHTechno specializes in website and mobile application development, offering innovative solutions to enhance your digital presence.">
+        <meta property="og:image" content="{{ asset('/images/og-image.jpeg') }}">
+        <meta property="og:url" content="{{ url()->current() }}">
+
+        <meta property="og:type" content="website">
+        <!-- website favicon  -->
+        <link rel="icon" href="{{ asset('/images/favicon.png') }}" type="image/png"/>
+
         @viteReactRefresh
         @vite('resources/js/app.jsx')
         @inertiaHead
